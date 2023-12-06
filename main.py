a690b4c97c76b1ea48e38715fdf3f654cf96ae96
Jakub Wolniak <kubawolniak@wp.pl>
2023-12-06 11:00:07 +0100

    some updates

--- main.py
+++ main.py
@@ -1,81 +1,99 @@
 import numpy as np
+import warnings
 from scipy.io import wavfile
 from scipy.fftpack import fft
-from scipy.signal import butter, sosfilt
-from matplotlib import pyplot as plt
 import os
 
-# Define a function to apply a bandpass filter
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    sos = butter(order, [low, high], analog=False, btype='band', output='sos')
-    return sos
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    sos = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = sosfilt(sos, data)
-    return y
-
 def iterate_files():
+    ans = []
     directory = 'train'
     iterator = 0
     for filename in os.listdir(directory):
         if filename.endswith('.wav'):
-            analysis(os.path.join(directory, filename), iterator)
+            ans.append(analysis(os.path.join(directory, filename), iterator))
             iterator += 1
+    return ans
 
 def analysis(path, iterator=0):
-    # Load the .wav file
-    sample_rate, data = wavfile.read(path)
+    # Ignore warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        # Load the .wav file
+        sample_rate, data = wavfile.read(path)
 
     # If the audio file has more than one channel (i.e., it's a stereo file), convert it to mono
     if len(data.shape) > 1:
         data = np.mean(data, axis=1)
 
-    # Apply a bandpass filter to the audio data
-    data = butter_bandpass_filter(data, 80, 255, sample_rate)
-
     # Normalize the audio data
     data = data / np.max(np.abs(data))
 
-    # Apply FFT
-    fft_out = fft(data)
+    # Divide the audio data into chunks
+    chunk_size = round(sample_rate)  # 1 second chunks
+    num_chunks = len(data) // chunk_size
 
-    # Calculate the absolute value of each FFT point
-    abs_fft = np.abs(fft_out)
+    # Classify each chunk
+    male_chunks = 0
+    female_chunks = 0
+    for i in range(num_chunks):
+        chunk = data[i*chunk_size:(i+1)*chunk_size]
 
-    # Only consider the first half of the frequencies and FFT output
-    abs_fft = abs_fft[:len(abs_fft)//2]
-    frequencies = np.fft.fftfreq(len(abs_fft)*2, 1.0/sample_rate)[:len(abs_fft)]
+        # Apply FFT
+        fft_out = fft(chunk)
 
-    # Find the peak frequency in the product spectrum
-    peak_frequency = abs(frequencies[np.argmax(abs_fft)])
+        # Calculate the absolute value of each FFT point
+        abs_fft = np.abs(fft_out)
 
-    # Classify based on the peak frequency
-    if 85 <= peak_frequency <= 180:
-        #print(peak_frequency)
-        print(iterator+1, "M")
-    elif 165 <= peak_frequency <= 255:
-        #print(peak_frequency)
-        print(iterator+1, "K")
+        # Only consider the first half of the frequencies and FFT output
+        abs_fft = abs_fft[:len(abs_fft)//2]
+        frequencies = np.fft.fftfreq(len(abs_fft)*2, 1.0/sample_rate)[:len(abs_fft)]
+
+        # Create a mask for frequencies between 50 and 400
+        mask = (85 <= frequencies) & (frequencies <= 255)
+
+        # Apply the mask to the frequencies and FFT output
+        frequencies = frequencies[mask]
+        abs_fft = abs_fft[mask]
+
+        # Find the peak frequency in the product spectrum
+        peak_frequency = abs(frequencies[np.argmax(abs_fft)])
+
+        # Classify based on the peak frequency
+        if 85 <= peak_frequency < 165:
+            male_chunks += 1
+        elif 165 <= peak_frequency <= 255:
+            female_chunks += 1
+
+    # Classify the whole track
+    if male_chunks > female_chunks:
+        return "M"
     else:
-        #print(peak_frequency)
-        print(iterator+1, "U")
+        return "K"
+
+def get_answer_from_name():
+    names = []
+    for filename in os.listdir('train'):
+        if filename.endswith('.wav'):
+            if filename[4] == 'M':
+                names.append('M')
+            else:
+                names.append('K')
+    return names
+
+def get_accuracy(answers, names):
+    count = 0
+    for i in range(len(answers)):
+        if answers[i] == names[i]:
+            count += 1
+    return count / len(answers)
 
 def main():
-    iterate_files()
+    answers = iterate_files()
+    names = get_answer_from_name()
+    print(get_accuracy(answers, names))
 
-# # Plot the spectrum
-# plt.figure(figsize=(14, 5))
-# plt.plot(frequencies, abs_fft)
-# #plt.plot(frequencies[:len(product)], product)
-# plt.xlabel('Frequency (Hz)')
-# plt.ylabel('Magnitude')
-# plt.title('Spectrum')
-# plt.xlim(0, 500)
-# plt.show()
+
+
 
 if __name__ == '__main__':
     main()
